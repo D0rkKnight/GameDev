efea19a30d41fbf297cee8a0d0be23980ae44a64
D0rkKnight <shouhanzen@gmail.com>
2020-06-15 13:39:11 -0700

    basic framework

--- Game/src/Main.java
+++ Game/src/Main.java
@@ -1,6 +1,22 @@
+import java.awt.Canvas;
+
+import javax.swing.JFrame;
 
 public class Main {
+	
+	static JFrame frame;
+	static Canvas canvas;
+	
 	public static void main(String[] args) {
 		System.out.println("hellow rold");
+		
+		frame = new JFrame();
+		
+		canvas = new Drawing();
+		canvas.setSize(1280, 720);
+		frame.add(canvas);
+		frame.pack();
+		frame.setVisible(true);
+		
 	}
 }
