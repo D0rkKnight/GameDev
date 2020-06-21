fd3b2d6f7750283fba08f35fdcffd100484a0120
Alethar <benjamingao99@gmail.com>
2020-06-20 21:20:54 -0700

    Made NPC abstract
    Reason: we will likely have both moving/stationary NPCs with text conversations, in addition to critters being NPCs (but not having any text conversations)

--- Game/src/Entities/NPC.java
+++ Game/src/Entities/NPC.java
@@ -1,5 +1,13 @@
 package Entities;
 
-public class NPC {
+import Wrappers.Position;
+import Wrappers.Sprites;
+
+public abstract class NPC extends Entity {
+
+	public NPC(int ID, Position position, Sprites sprites) {
+		super(ID, position, sprites);
+		// TODO Auto-generated constructor stub
+	}
 
 }
