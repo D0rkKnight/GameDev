c243006a16d5da9a4ec063fc6afc2df38b8597ef
Alethar <benjamingao99@gmail.com>
2020-08-12 23:07:35 -0700

    Map and Tileset (test)

---
+++ Game/assets/TestTiles.tsx
@@ -0,0 +1,294 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.4" tiledversion="1.4.2" name="TestTiles" tilewidth="8" tileheight="8" tilecount="256" columns="16">
+ <image source="tset1.png" trans="3b35ff" width="128" height="128"/>
+ <tile id="0" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="1" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="2" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="3" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="4" type="0">
+  <properties>
+   <property name="HammerShape" value="2"/>
+  </properties>
+ </tile>
+ <tile id="5" type="0">
+  <properties>
+   <property name="HammerShape" value="1"/>
+  </properties>
+ </tile>
+ <tile id="6" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="7" type="0"/>
+ <tile id="8" type="0"/>
+ <tile id="16" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="17" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="18" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="19" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="20" type="0">
+  <properties>
+   <property name="HammerShape" value="4"/>
+  </properties>
+ </tile>
+ <tile id="21" type="0">
+  <properties>
+   <property name="HammerShape" value="3"/>
+  </properties>
+ </tile>
+ <tile id="22" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="23" type="0"/>
+ <tile id="24" type="0"/>
+ <tile id="32" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="33" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="34" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="35" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="36" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="37" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="38" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="39" type="0"/>
+ <tile id="40" type="0"/>
+ <tile id="48" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="49" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="50" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="51" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="52" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="53" type="0">
+  <properties>
+   <property name="HammerShape" value="0"/>
+  </properties>
+ </tile>
+ <tile id="54" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="55" type="0"/>
+ <tile id="56" type="0"/>
+ <tile id="64" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="65" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="66" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="67" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="68" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="69" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="70" type="0">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="71" type="0"/>
+ <tile id="72" type="0"/>
+ <tile id="80">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="81">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="82">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="83">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="84">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="85">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="86">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="96">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="97">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="98">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="99">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="100">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="101">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="102">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="112">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="113">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="114">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="115">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="116">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="117">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+ <tile id="118">
+  <properties>
+   <property name="HammerShape" value=""/>
+  </properties>
+ </tile>
+</tileset>
